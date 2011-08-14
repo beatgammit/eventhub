0c62185b8015b8858591e703d4226c90b94ad6f1
Jameson Little <t.jameson.little@gmail.com>
2011-08-14 12:31:27 -0600

    Extended tests some

--- test/test.js
+++ test/test.js
@@ -23,7 +23,7 @@
 
 		EventHub.emit('single', 'Some data...');
 		timeout = setTimeout(function () {
-			var count = 0, timeout, interval;
+			var count = 0, timeout, interval, stopAt = 2;
 
 			if (!pass) {
 				return cb('Event not emitted =\'(');
@@ -37,9 +37,9 @@
 
 				count += 1;
 
-				if (count === 2) {
+				if (count === stopAt) {
 					EventHub.removeListener('multiple', multipleListener);
-				} else if (count > 2) {
+				} else if (count > stopAt) {
 					clearTimeout(timeout);
 					clearInterval(interval);
 					return cb('Too many events fired, which means removeListener didn\'t work =\'(');
@@ -52,7 +52,7 @@
 
 			timeout = setTimeout(function () {
 				clearInterval(interval);
-				if (count !== 2) {
+				if (count !== stopAt) {
 					return cb('Event not fired the correct number of times =\'(');
 				}
 
@@ -78,7 +78,7 @@
 
 		emitter.emit('single', 'Some data...');
 		timeout = setTimeout(function () {
-			var count = 0, timeout, interval;
+			var count = 0, timeout, interval, stopAt = 2;
 
 			if (!pass) {
 				return cb('Event not emitted =\'(');
@@ -92,9 +92,9 @@
 
 				count += 1;
 
-				if (count === 2) {
+				if (count === stopAt) {
 					EventHub.removeListener(emitterName, 'multiple', multipleListener);
-				} else if (count > 2) {
+				} else if (count > stopAt) {
 					clearTimeout(timeout);
 					clearInterval(interval);
 					return cb('Too many events fired, which means removeListener didn\'t work =\'(');
@@ -107,7 +107,7 @@
 
 			timeout = setTimeout(function () {
 				clearInterval(interval);
-				if (count !== 2) {
+				if (count !== stopAt) {
 					return cb('Event not fired the correct number of times =\'(');
 				}
 
@@ -116,27 +116,155 @@
 		}, 10);
 	}
 
-/*
 	function testExtended(cb) {
-		console.log('Testing extended functionality');
-		cb();
+		var listener1Called = false,
+			listener2Called = false;
+
+		function listener1() {
+			listener1Called = true;
+		}
+		function listener2() {
+			listener2Called = true;
+		}
+
+		console.log('Testing multiple listeners');
+
+		EventHub.once('once-multiple', listener1);
+		EventHub.once('once-multiple', listener2);
+
+		EventHub.emit('once-multiple');
+
+		setTimeout(function () {
+			var stopAt = 2;
+
+			function listener1() {
+				listener1Called += 1;
+			}
+			function listener2() {
+				listener2Called += 1;
+			}
+
+			if (!listener1Called || !listener2Called) {
+				return cb('Both listeners were not called =\'(');
+			}
+
+			if (EventHub.listeners('once-multiple').length > 0) {
+				return cb('Both listeners called, but not removed');
+			}
+
+			listener1Called = 0;
+			listener2Called = 0;
+
+			EventHub.on('on-multiple', listener1);
+			EventHub.on('on-multiple', listener2);
+
+			EventHub.emit('on-multiple');
+			EventHub.emit('on-multiple');
+
+			setTimeout(function () {
+				if (listener1Called !== stopAt || listener2Called !== stopAt) {
+					return cb('Events not called the correct number of times');
+				}
+
+				if (EventHub.listeners('on-multiple').length !== 2) {
+					return cb('Incorrect number of listeners');
+				}
+
+				EventHub.removeAllListeners('on-multiple');
+
+				if (EventHub.listeners('on-multiple').length > 0) {
+					return cb('Listeners not removed');
+				}
+
+				cb();
+			}, 50);
+		}, 50);
 	}
 
+	function testExtendedResource(cb) {
+		var listener1Called = false,
+			listener2Called = false,
+			emitter = new EventEmitter(),
+			emitterName = 'multiple';
+
+		function listener1() {
+			listener1Called = true;
+		}
+		function listener2() {
+			listener2Called = true;
+		}
+
+		console.log('Testing multiple listeners on a resource');
+
+		EventHub.register(emitterName, emitter);
+
+		EventHub.once(emitterName, 'once-multiple', listener1);
+		EventHub.once(emitterName, 'once-multiple', listener2);
+
+		emitter.emit('once-multiple');
+
+		setTimeout(function () {
+			var stopAt = 2;
+
+			function listener1() {
+				listener1Called += 1;
+			}
+			function listener2() {
+				listener2Called += 1;
+			}
+
+			if (!listener1Called || !listener2Called) {
+				return cb('Both listeners were not called =\'(');
+			}
+
+			if (EventHub.listeners('once-multiple').length > 0) {
+				return cb('Both listeners called, but not removed');
+			}
+
+			listener1Called = 0;
+			listener2Called = 0;
+
+			EventHub.on(emitterName, 'on-multiple', listener1);
+			EventHub.on(emitterName, 'on-multiple', listener2);
+
+			emitter.emit('on-multiple');
+			emitter.emit('on-multiple');
+
+			setTimeout(function () {
+				if (listener1Called !== stopAt || listener2Called !== stopAt) {
+					return cb('Events not called the correct number of times');
+				}
+
+				if (EventHub.listeners(emitterName, 'on-multiple').length !== 2) {
+					return cb('Incorrect number of listeners');
+				}
+
+				EventHub.removeAllListeners(emitterName, 'on-multiple');
+
+				if (EventHub.listeners(emitterName, 'on-multiple').length > 0) {
+					return cb('Listeners not removed');
+				}
+
+				cb();
+			}, 50);
+		}, 50);
+	}
+/*
 	function testBad(cb) {
 		console.log('Testing bad values');
 		cb();
 	}
 */
 
-	tests = [testBasic, testBasicResources/*, testExtended, testBad*/];
+	tests = [testBasic, testBasicResources, testExtended, testExtendedResource/*, testBad*/];
 
 	forEachAsync(tests, function (next, fn) {
 		fn(function (err) {
 			if (err) {
-				console.log('Test failed:' + err);
+				console.log('Test failed:', err);
 				fail = true;
 			} else {
-				console.log('Test passed =\'D');
+				console.log('Test passed =D');
 			}
 
 			console.log();
